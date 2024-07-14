dde1c1b62e577081a6555cbfb492df8950116bbf
79096 <a.malakhinsky@gmail.com>
2024-07-14 19:47:07 +0300

    -

---
+++ catalog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from catalog.apps import NewappConfig
+from catalog.views import render_home, render_contacts
+
+app_name = NewappConfig.name
+# urlpatterns = [path('', include("catalog.urls", namespace="catalog"))]
+urlpatterns = [path('', render_home, name='home'), path('contacts/', render_contacts, name='contacts')]
--- newapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from newapp.apps import NewappConfig
-from newapp.views import render_home, render_contacts
-
-app_name = NewappConfig.name
-# urlpatterns = [path('', include("newapp.urls", namespace="newapp"))]
-urlpatterns = [path('', render_home, name='home'), path('contacts/', render_contacts, name='contacts')]
