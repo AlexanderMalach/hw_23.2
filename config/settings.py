732b7e8a82e9e9c30c9c9c596f8ee63ca4a82917
79096 <a.malakhinsky@gmail.com>
2024-07-18 14:49:00 +0300

    -

--- config/settings.py
+++ config/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "hw_shop",
+        "NAME": "hw_products_catagory",
         "USER": "postgres",
         "PASSWORD": "1991",
         "HOST": "127.0.0.1",
