94b0342082ab6b928ad190462bf7822ab60c3336
79096 <a.malakhinsky@gmail.com>
2024-07-18 20:07:50 +0300

    -

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from catalog.apps import NewappConfig
-from catalog.views import render_home, render_contacts
+
 
 app_name = NewappConfig.name
 # urlpatterns = [path('', include("catalog.urls", namespace="catalog"))]
--- catalog/views.py
+++ catalog/views.py
@@ -1,16 +1,16 @@
-from django.shortcuts import render
-
-
-def render_home(request):
-    return render(request, "home.html")
-
-
-def render_contacts(request):
-    if request.method == "POST":
-        name = request.POST.get("name")
-        phone = request.POST.get("phone")
-        message = request.POST.get("message")
-        print(
-            f"Имя пользователя: {name}\nНомер телефона: {phone}\nСообщения от пользователя: {message}"
-        )
-    return render(request, "contacts.html")
+# from django.shortcuts import render
+#
+#
+# def render_home(request):
+#     return render(request, "home.html")
+#
+#
+# def render_contacts(request):
+#     if request.method == "POST":
+#         name = request.POST.get("name")
+#         phone = request.POST.get("phone")
+#         message = request.POST.get("message")
+#         print(
+#             f"Имя пользователя: {name}\nНомер телефона: {phone}\nСообщения от пользователя: {message}"
+#         )
+#     return render(request, "contacts.html")
